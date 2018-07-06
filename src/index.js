e2223c4031f95976a61561cfed40900d6e7e6d11
ayushpratap <ayushs56@gmail.com>
2018-07-07 01:36:37 +0530

    Added support for morgan logging for logging of all http request and responses

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 var express = require('express'),
 	search = require('./includes/search'),
 	call = require('./includes/call');
-var morgon = require('morgan');
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
 var app = express();
 var port = 8000;
+
+var logStream = fs.createWriteStream(path.join(__dirname, 'log/logfile.log'),{flag: 'a'});
+
+app.use(morgan('combined',{stream : logStream}));
 app.use('/search',search);
 app.use('/call',call);
 
