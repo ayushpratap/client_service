a91670cbfc95e1d9fc0029f1b04a3c57c31b19d5
Ayush Pratap <ayushs56@gmail.com>
2018-07-28 13:59:56 +0530

    corrected https server arguments

--- src/server.js
+++ src/server.js
@@ -9,16 +9,16 @@ const app = require('./app/app');
 const CONFIG = require('./config/config');
 const winston = require('./middleware/winston');
 const https = require('https');
-const server = express(); // Get an instace of express
+const http = require('http');
 const fs = require('fs');
+const server = express(); // Get an instace of express
 
-const privateKey  = fs.readFileSync('./certs/server.key');
-const certificate = fs.readFileSync('./certs/server.crt');
-const credentials = {key: privateKey, cert: certificate};
-const httpsServer = https.createServer(credentials, app);
+var privateKey  = fs.readFileSync('./certs/server.key');
+var certificate = fs.readFileSync('./certs/server.crt');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, server);
 
 winston.info('Starting server at port :' + CONFIG.port);
 server.all('/', app); // Redirect all the requests to app
 httpsServer.listen(CONFIG.port);  // Server running at port = process.env.PORT
-// server.listen(CONFIG.port); // Server running at port = process.env.PORT
 winston.info('Server running at port :' + CONFIG.port);
