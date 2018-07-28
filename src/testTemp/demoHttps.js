29bc674267d184addb2b646b12d81fe4b4b2f4a3
Ayush Pratap <ayushs56@gmail.com>
2018-07-28 13:26:46 +0530

    intermediate commit

--- src/testTemp/demoHttps.js
+++ src/testTemp/demoHttps.js
@@ -1,25 +1,3 @@
-/*const express = require('express')
-const fs = require('fs');
-const https = require('https');
-
-var httpsOptions = {
-    key: fs.readFileSync('../certs/server.key')
-      , cert: fs.readFileSync('../certs/server.crt')
-}
-
-const appHttp = express.createServer();
-const appHttps = express.createServer(httpsOptions);
-
-appHttp.get('/',function(req,res){
-  res.send('HTTP GET PORT : 8000');
-});
-
-appHttps.get('/',function(req,res){
-  res.send('HTTPS GET PORT : 8443');
-});
-appHttp.listen(8000);
-appHttps.listen(8443);
-*/
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
