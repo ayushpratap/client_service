76ab23652a7d94da85842f44690f041445071248
Ayush Pratap <ayushs56@gmail.com>
2018-07-28 13:15:55 +0530

    running and tested https server demo

--- src/testTemp/demoHttps.js
+++ src/testTemp/demoHttps.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const fs = require('fs');
 const https = require('https');
 
@@ -19,3 +19,27 @@ appHttps.get('/',function(req,res){
 });
 appHttp.listen(8000);
 appHttps.listen(8443);
+*/
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('../certs/server.key');
+var certificate = fs.readFileSync('../certs/server.crt');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+app.get('/',function(req,res){
+	res.send('HELLO WORLD');
+});
+
+httpServer.listen(8000);
+console.log('HTTP server is running at port : 8000');
+httpsServer.listen(8443);
+console.log('HTTPS server is running at port : 8443');
