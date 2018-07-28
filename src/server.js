a5dab323e087150fcf7b7af9886e1cca421c40fe
Ayush Pratap <ayushs56@gmail.com>
2018-07-28 13:26:34 +0530

    moved http server to https server

--- src/server.js
+++ src/server.js
@@ -8,10 +8,17 @@ const express = require('express');
 const app = require('./app/app');
 const CONFIG = require('./config/config');
 const winston = require('./middleware/winston');
-
+const https = require('https');
 const server = express(); // Get an instace of express
+const fs = require('fs');
+
+const privateKey  = fs.readFileSync('./certs/server.key');
+const certificate = fs.readFileSync('./certs/server.crt');
+const credentials = {key: privateKey, cert: certificate};
+const httpsServer = https.createServer(credentials, app);
 
 winston.info('Starting server at port :' + CONFIG.port);
 server.all('/', app); // Redirect all the requests to app
-server.listen(CONFIG.port); // Server running at port = process.env.PORT
+httpsServer.listen(CONFIG.port);  // Server running at port = process.env.PORT
+// server.listen(CONFIG.port); // Server running at port = process.env.PORT
 winston.info('Server running at port :' + CONFIG.port);
