29e05cac87ca03874a16df51fae06408e6dab312
Ayush Pratap <ayushs56@gmail.com>
2019-01-08 15:43:18 +0530

    correcting the flcon http server url

--- Falcon/src/config/config.js
+++ Falcon/src/config/config.js
@@ -5,6 +5,6 @@ let CONFIG = {};
 CONFIG.app = process.env.APP || 'dev';
 CONFIG.tcp_server_port = process.env.TCP_SERVER_PORT || '9001';
 CONFIG.tcp_server_host = process.env.TCP_SERVER_HOST || '3.85.228.22';
-CONFIG.client_service_url = process.env.CLIENT_SERVICE_URL || 'https://127.0.0.1:8443/';
+CONFIG.client_service_url = process.env.CLIENT_SERVICE_URL || 'https://127.0.0.1:8443';
 
 module.exports = CONFIG;
