993ec3f248365547d235886b5d66b430f3d107f8
Ayush Pratap <ayushs56@gmail.com>
2019-01-09 17:20:10 +0530

    changing tcp server host

--- Falcon/src/config/config.js
+++ Falcon/src/config/config.js
@@ -4,7 +4,7 @@ let CONFIG = {};
 
 CONFIG.app = process.env.APP || 'dev';
 CONFIG.tcp_server_port = process.env.TCP_SERVER_PORT || '9001';
-CONFIG.tcp_server_host = process.env.TCP_SERVER_HOST || '3.85.228.22';
+CONFIG.tcp_server_host = process.env.TCP_SERVER_HOST || 'ec2-3-80-153-122.compute-1.amazonaws.com';
 CONFIG.client_service_url = process.env.CLIENT_SERVICE_URL || 'https://127.0.0.1:8443';
 
 module.exports = CONFIG;
