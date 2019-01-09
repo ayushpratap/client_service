bf2514d082ff317591923e1557bccacff4651c70
Ayush Pratap <ayushs56@gmail.com>
2019-01-09 17:47:48 +0530

    no message

--- Falcon/src/clients.js
+++ Falcon/src/clients.js
@@ -4,46 +4,66 @@ var request = require('request');
 var CONFIG = require('./config/config');
 const JsonSocket = require('json-socket');
 var options = {};
+const logger = CONFIG.logger;
 
+// Create client socket
+logger.info('Creating client socket');
 const tcpClient = net.createConnection(CONFIG.tcp_server_port,CONFIG.tcp_server_host);
 
+//  Handle connect event
 tcpClient.on('connect',function(){
-    console.log("Connected to the server at "+CONFIG.tcp_server_host+":"+CONFIG.tcp_server_port);
-    console.log("TCP client info "+tcpClient.address().address);
+    logger.info("Connected to the server at "+CONFIG.tcp_server_host+":"+CONFIG.tcp_server_port);
+    logger.info("TCP client info "+tcpClient.address().address);
 });
 
-
+//  Handle data event
 tcpClient.on('data',function(data){
+    logger.info('Data recevied');
+
     // Convert the Buffer to JSON object
     let reqInfo = JSON.parse(data.toString());
-
+    logger.debug(reqInfo);
     // Get the route of the request
     switch(reqInfo.route){
         case '/':
-            console.log(reqInfo);
-            // Make a HTTP request to client service
+            logger.info("Case : '/'");
+            
+            // Set the options for HTTPS request
             options.method              = "GET";
             options.url                 = CONFIG.client_service_url+reqInfo.route;
             options.rejectUnauthorized  = false;
             options.requestCert         = false;
             options.body                = reqInfo.data;
+
+            logger.debug(options);
+            
+            // Make HTTPS request
             request(options,function(error,res,body){
+                logger.info('Response for HTTPS request');
                 options = {};
-                console.log(body);
+                logger.debug('Cleared HTTPS options',options);
+                logger.info(body);
             });
         break;
         case '/api/makeCall':
-            // Make a HTTP request to client service
-            console.log(reqInfo);
+            logger.info("Case : /api/makeCall");
+
+            // Set the options for HTTPS request
             options.method              = "POST";
             options.url                 = CONFIG.client_service_url+reqInfo.route;
             options.json                = true;
             options.rejectUnauthorized  = false;
             options.requestCert         = false;
             options.body                = reqInfo.data;
+
+            logger.debug(options);
+
+            //  Make HTTPS request
             request(options,function(error,res,body){
+                logger.info('Response for HTTPS request');
                 options = {};
-                console.log(body);
+                logger.debug('Cleared HTTPS options', options);
+                logger.info(body);
             });
         break;
         case '/api/makeCallMulti':
--- Falcon/src/config/config.js
+++ Falcon/src/config/config.js
@@ -1,10 +1,71 @@
 require('dotenv').config();
-
+var appRoot = require('app-root-path');
+const winston = require('winston');
 let CONFIG = {};
+//------------------------------------------------------------------------------
+CONFIG.app 					= process.env.APP 					|| 'dev';
+CONFIG.tcp_server_port 		= process.env.TCP_SERVER_PORT 		|| '9001';
+CONFIG.tcp_server_host 		= process.env.TCP_SERVER_HOST 		|| 'ec2-3-80-153-122.compute-1.amazonaws.com';
+CONFIG.client_service_url 	= process.env.CLIENT_SERVICE_URL 	|| 'https://127.0.0.1:8443';
+CONFIG.env 					= process.env.ENV					|| 'dev';
+//------------------------------------------------------------------------------
 
-CONFIG.app = process.env.APP || 'dev';
-CONFIG.tcp_server_port = process.env.TCP_SERVER_PORT || '9001';
-CONFIG.tcp_server_host = process.env.TCP_SERVER_HOST || 'ec2-3-80-153-122.compute-1.amazonaws.com';
-CONFIG.client_service_url = process.env.CLIENT_SERVICE_URL || 'https://127.0.0.1:8443';
+// Create logger for logging
+const logger = winston.createLogger({
+		transports:[
+		new winston.transports.File({
+										format: winston.format.combine(
+												winston.format.label({
+														label: CONFIG.env
+													}),												
+												winston.format.timestamp({
+													format: 'HH:mm:ss DD-YYYY-MM'
+												}),
+												winston.format.json()
+											),
+										filename: appRoot+'/logs/error.log', 
+										level: 'error'
+									}),
+		new winston.transports.File({
+										format: winston.format.combine(
+												winston.format.label({
+														label: CONFIG.env
+													}),
+												winston.format.timestamp({
+													format: 'HH:mm:ss DD-YYYY-MM'
+												}),
+												winston.format.json(),
+											),										
+										filename: appRoot+'/logs/combined.log',
+										level: 'silly'
+									})
+	]
+});
 
+// If application is running in dvelopemnt environment then show logs to console
+// else do not show logs on console
+if(process.env.ENV === 'prod'){
+	logger.add(new winston.transports.Console({
+		format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple()
+			),
+		level: 'info'
+	}));
+}
+
+if(process.env.ENV === 'dev'){
+	logger.add(new winston.transports.Console({
+		format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple()
+			),
+		level: 'silly'
+	}));
+}
+
+// Add the logger instance to the global CONFIG
+//------------------------------------------------------------------------------
+CONFIG.logger = logger;
+//------------------------------------------------------------------------------
 module.exports = CONFIG;
