da90e9c5564e95616cefc176298c237fa31c6b17
Ashish <rastogiashish52@gmail.com>
2019-07-04 15:02:02 +0530

    adding REFER header

--- Saturn/src/middleware/sip.middleware.js
+++ Saturn/src/middleware/sip.middleware.js
@@ -16,8 +16,8 @@ stack.startStack = function(){
     let sip_port = CONFIG.sip_port;
     logger.info(sip_port);
     sip.start({protocol:'UDP',address:IP.address(),port:sip_port},(rs)=>{
-        logger.info("[%s] , %o",__file,rs);
-        // logger.info("[%s] , %o",__file,rs.method); 
+        //logger.info("[%s] , %o",__file,rs);
+       // logger.info("[%s] , %o",__file,rs.method); 
         switch(rs.method)
         {
             case 'NOTIFY':
@@ -29,7 +29,6 @@ stack.startStack = function(){
                 case 'OPTIONS':
                 logger.info('[%s], CASE : OPTIONS ',__file)
                 sip.send(logger.info('[%s], %o',__file,sip.makeResponse(rs,200,'OK')));
-                
                 break;
             default:
                 logger.info("%o",rs.method);
@@ -40,8 +39,8 @@ stack.startStack = function(){
 
 register = function(sip){
     logger.info("[%s] , stack.register",__file);
-    let domain      = CONFIG.sip_domain
-    let localExnt   = CONFIG.sip_local_extn
+    let domain      = CONFIG.sip_domain;
+    let localExnt   = CONFIG.sip_local_extn;
     let transport   = CONFIG.sip_transport;
     let macAddr     = CONFIG.mac_addr;
     let MaxFowards  = CONFIG.max_forwards;
@@ -57,28 +56,32 @@ register = function(sip){
     let contact     = [{name:name,uri:'sip:'+localExnt+'@'+IP.address()+':'+sip_port+';transport='+transport}];
     let expires     = 3600;
     let privacy     = "none";
-    let UserAgent   = "NECSDT800_ITY-8LDX/4.3.18.14_"+macAddr;
+    let UserAgent   = "NECSDT700_ITL-12G/3.0.21.19_"+macAddr;
+    let version     = "2.0";
     let via         = [{version:version,protocol:'UDP',host:IP.address(),port:sip_port,params:{branch:cryptoRand({length:16})}}];
-
+    let content_length = 0;
+    
 
     // Create the packet
     let REGISTER = {
-        method:'REGISTER',
-        uri:registerUri,
-        headers:{
-            via:via,
-            'Max-Forwards':MaxFowards,
-            from:from,
-            to:to,
-            'call-id':call_id,
-            cseq:cseq,
-            'Allow':allow,
-            contact:contact,
-            'Expires':expires,
-            'Privacy':privacy,
-            'User-Agent':UserAgent
+        method  :   'REGISTER',
+        uri     :   registerUri,
+        version :   version,
+        headers :   {
+                via             :   via,
+                from            :   from,
+                to              :   to,
+                cseq            :   cseq,
+                contact         :   contact,
+                'call-id'       :   call_id,
+                'Max-Forwards'  :   MaxFowards,
+                'Allow'         :   allow,
+                'Expires'       :   expires,
+                'Privacy'       :   privacy,
+                'User-Agent'    :   UserAgent,
         }
     };
+
     sip.send(REGISTER,(rs)=>{
         switch(rs.status)
         {
@@ -94,6 +97,7 @@ register = function(sip){
         }
     });
 }
+
 stack.makeCall = function(source,destination,callback){
     logger.info("[%s] , stack.makeCall",__file);
     sip.start({protocol:'UDP',address:IP.address(),port:sip_port},(rs)=>{
@@ -116,11 +120,57 @@ stack.makeCall = function(source,destination,callback){
                 logger.info("%o",rs.method);
         }
     });
-    invite(sip,source,destination);
+    refer(sip,source,destination);
 }
 
-invite = function(sip,source,destination){
+refer = function(sip,source,destination){
+    logger.info("[%s] , stack.refer",__file);
+    let domain      = CONFIG.sip_domain;
+    let localExnt   = CONFIG.sip_local_extn;
+    let transport   = CONFIG.sip_transport;
+    let macAddr     = CONFIG.mac_addr;
+    let MaxFowards  = CONFIG.max_forwards;
+    let regisAddr   = CONFIG.sip_regis_addr;
+    let name        = CONFIG.sip_local_name;
+    let sip_port    = CONFIG.sip_port;
+    let registerUri = 'sip:'+regisAddr;
+    let from        = {name:name,uri:'sip:'+localExnt+'@'+domain,params:{tag:cryptoRand({length:10})}};
+    let to          = {name:source,uri:'sip:'+source+'@'+domain};
+    let referTarget = {name:destination,uri:'sip:'+destination+'@'+domain};
+    let call_id     = cryptoRand({length:14})+"@"+domain;
+    let cseq        = {method:'REGISTER',seq:cryptoRand({length:9,characters: '1234567890'})};
+    let allow       = "INVITE, ACK, CANCEL, BYE, NOTIFY, REFER, OPTIONS, UPDATE";
+    let contact     = [{name:name,uri:'sip:'+localExnt+'@'+IP.address()+':'+sip_port+';transport='+transport}];
+    let expires     = 3600;
+    let privacy     = "none";
+    let UserAgent   = "NECSDT700_ITL-12G/3.0.21.19_"+macAddr;
+    let version     = "2.0";
+    let via         = [{version:version,protocol:'UDP',host:IP.address(),port:sip_port,params:{branch:cryptoRand({length:16})}}];
+    let content_length = 0;
+
+    let REFER = {
+        method  :   'REFER',
+        uri     :   to,
+        params  :   {gr:cryptoRand({length:9})},
+        version :   version,
+        headers :   {
+                via             :   via,
+                from            :   from,
+                to              :   to,
+                cseq            :   cseq,
+                contact         :   contact,
+                'refer-to'      :   referTarget,
+                'call-id'       :   call_id,
+                'Max-Forwards'  :   MaxFowards,
+                'Allow'         :   allow,
+                'Expires'       :   expires,
+                'Privacy'       :   privacy,
+                'User-Agent'    :   UserAgent,
+        }
+    };
+
 
 }
+
 
 module.exports = stack;
